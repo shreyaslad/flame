db4649b5fe9f8a8dd004d41911be6ce081e58013
Shreyas Lad <shadowtemplates@gmail.com>
2019-09-17 21:19:26 -0700

    Update mem.h

--- libc/mem.h
+++ libc/mem.h
@@ -9,4 +9,18 @@ void memory_set(u8* dest, u8 val, u32 len);
 // Deprecated kmalloc implementation
 u32 kmalloc(u32 size, int align, u32* phys_addr);
 
+char memory[20000];
+
+struct block {
+	size_t size;
+	int free;
+	struct block *next;
+};
+
+void initialize();
+void split(struct block* fitting_slot, size_t size);
+void *malloc(size_t bytes);
+void merge();
+void free(void* ptr);
+
 #endif
