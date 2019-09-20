7c3a8a7f8f343e1517ed4c6fb0d865f223672cca
Shreyas Lad <shadowtemplates@gmail.com>
2019-09-20 14:27:46 -0700

    Update int.c

--- libc/int.c
+++ libc/int.c
@@ -1,1 +1,13 @@
+/*
+  int.c
+  Copyright Shreyas Lad (PenetratingShot) 2019
+  MIT License
+  
+  Contains functions for handy integer conversions
+*/
 
+#include "int.h"
+
+int octtodec(int value) {
+  
+}
