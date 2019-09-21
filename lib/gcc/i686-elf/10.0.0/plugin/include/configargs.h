ae8ad6a7c2d9a7c5c9e2e4f335105e20b874555a
Shreyas Lad <shadowtemplates@gmail.com>
2019-09-21 10:28:28 -0700

    removed stuff

--- lib/gcc/i686-elf/10.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=i686-elf --prefix=/home/meemr/opt/cross --disable-nls --enable-languages=c,c++ --without-headers";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
