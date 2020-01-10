52893d138978a281d2d75547cdcb2352ab79eaf1
Penetratingshot <shadowtemplates@gmail.com>
2020-01-09 23:10:30 -0800

    arrow keys

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -8,6 +8,8 @@
 
 #define RIGHT_ARROW 0x4D
 #define LEFT_ARROW 0x4B
+#define UP_ARROW 0x48
+#define DOWN_ARROW 0x50
 
 #define KEYUPOFFSET 0x80 //128
 
@@ -19,6 +21,7 @@ int coutkey = 0;
 int shift = 0; // is shift key pressed
 
 bool canType = true;
+uint8_t state = 0;
 
 char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
     "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
@@ -43,10 +46,32 @@ char sc_ascii_uppercase[] = { ' ', ' ', '!', '@', '#', '$', '%', '^',
 static void keyboard_callback(registers_t* regs) {
 
     uint8_t scancode = port_byte_in(0x60);
-	if (canType) {
-		logic(scancode);
-	} else {
 
+	// TODO: pressing the arrow key still increments coutkey
+
+	if (scancode == 224) {
+		state = 1;
+	} else if (scancode == RIGHT_ARROW && state == 1) {
+		state = 0;
+		
+		int oldOffset = get_cursor_offset();
+		int currentCol = get_offset_col(oldOffset);
+		int currentRow = get_offset_row(oldOffset);
+
+		int offset = get_offset(currentCol + 1, currentRow);
+		set_cursor_offset(offset);
+	} else if (scancode == LEFT_ARROW && state == 1) {
+		state = 0;
+
+		int oldOffset = get_cursor_offset();
+		int currentCol = get_offset_col(oldOffset);
+		int currentRow = get_offset_row(oldOffset);
+
+		int offset = get_offset(currentCol - 1, currentRow);
+		set_cursor_offset(offset);
+	} else {
+		if (canType)
+			logic(scancode);
 	}
 
     UNUSED(regs);
@@ -60,7 +85,7 @@ void logic(uint8_t scancode) {
 		scancode -= KEYUPOFFSET;
 	}
 
-	if (scancode > SC_MAX) return; // Typing a key that somehow doesn't exist
+	if (scancode > SC_MAX) return;
 
 	// Prevent user from deleting text that they didn't place
 	if (strcmp(sc_name[scancode], "Backspace") == 0 && keyUp == false) {
@@ -69,8 +94,7 @@ void logic(uint8_t scancode) {
 			kprint_backspace();
 
 			coutkey--;
-		}
-		else {
+		} else {
 			return;
 		}
 	} else if (strcmp(sc_name[scancode], "Enter") == 0 && keyUp == false) {
@@ -79,57 +103,29 @@ void logic(uint8_t scancode) {
 		key_buffer[0] = '\0';
 
 		coutkey = 0;
-	}
-	else if (strcmp(sc_name[scancode], "LShift") == 0 && keyUp == false) {
+	} else if (strcmp(sc_name[scancode], "LShift") == 0 && keyUp == false) {
 		shift = 1;
-	}
-	else if (strcmp(sc_name[scancode], "RShift") == 0 && keyUp == false) {
+	} else if (strcmp(sc_name[scancode], "RShift") == 0 && keyUp == false) {
 		shift = 1;
-	}
-	else if (strcmp(sc_name[scancode], "LShift") == 0 && keyUp == true) {
+	} else if (strcmp(sc_name[scancode], "LShift") == 0 && keyUp == true) {
 		shift = 0;
-	}
-	else if (strcmp(sc_name[scancode], "RShift") == 0 && keyUp == true) {
+	} else if (strcmp(sc_name[scancode], "RShift") == 0 && keyUp == true) {
 		shift = 0;
-	}
-	else if (scancode == LEFT_ARROW && keyUp == false) {
-		// TODO: insert characters into key_buffer whenever cursor pos is changed
-
-		if (coutkey > 0) {
-			int offset = get_cursor_offset();
-			set_cursor_offset(get_offset(get_offset_col(offset - 1), get_offset_row(offset)));
-		}
-		else {
-			// don't change the cursor pos
-		}
-	}
-	else if (scancode == RIGHT_ARROW && keyUp == false) {
-		
-	}
-	else if (strcmp(sc_name[scancode], "Spacebar") == 0 && keyUp == false) {
+	} else if (strcmp(sc_name[scancode], "Spacebar") == 0 && keyUp == false) {
 		strcat(key_buffer, " ");
 		kprint(" ");
 		coutkey++;
-	}
-	// the arrow keys aren't recognized but they print a scancode larger than 255 :/
-	else if (scancode == RIGHT_ARROW) {
-		int oldOffset = get_cursor_offset();
-		int currentCol = get_offset_col(oldOffset);
-		int currentRow = get_offset_row(oldOffset);
-
-		int offset = get_offset(currentCol + 1, currentRow);
-		set_cursor_offset(offset);
-	}
-	else if (scancode == LEFT_ARROW) {
-		int oldOffset = get_cursor_offset();
-		int currentCol = get_offset_col(oldOffset);
-		int currentRow = get_offset_row(oldOffset);
-
-		int offset = get_offset(currentCol - 1, currentRow);
-		set_cursor_offset(offset);
+	} else if (scancode + 128 == 224) {
+		if (state == 0) {
+			state = 1;
+		} else {
+			state = 0;
+		}
+	} else if (scancode == 77 && state == 1 && keyUp == false) {
+		state = 0;
+		sprint("r");
 	}
 	else {
-		
 		if (shift == 0) {
 			if (!keyUp) {
 				if (scancode < SC_MAX) {
@@ -141,8 +137,7 @@ void logic(uint8_t scancode) {
 					kprint(str);
 				}
 			}
-		}
-		else {
+		} else {
 			if (!keyUp) {
 				coutkey++;
 				if (scancode < SC_MAX) {
@@ -154,7 +149,7 @@ void logic(uint8_t scancode) {
 				}
 			}
 		}
-		
+	
 	}
 }
 
